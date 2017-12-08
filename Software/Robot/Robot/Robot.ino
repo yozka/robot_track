2c4965f563e9585e89735851d4c9ae7da05842a1
yozka <yozka@tigraha.com>
2017-12-08 23:38:03 +0500

    Добавление платы

--- Software/Robot/Robot/Robot.ino
+++ Software/Robot/Robot/Robot.ino
@@ -1,25 +1,19 @@
-/*
- Name:		Robot.ino
- Created:	08.12.2017 0:59:48
- Author:	Yozka
-*/
 #include "rtMotor.h"
 
 AMotor motor;
 
-// the setup function runs once when you press reset or power the board
+
 void setup() 
 {
-	//digitalWrite(0, 0);
 	motor.begin();
+
 }
 
-// the loop function runs over and over again until power down or reset
+
 void loop() 
 {
-	motor.speed(100, 100);
-	motor.stop();
-	motor.speedM1(100);
-	motor.speedM2(-100);
-
+	motor.setSpeed(400, 400);
+	delay(5000);
+	motor.setSpeed(-200, -200);
+	delay(2000);
 }
